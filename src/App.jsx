1ee002695df9a80c00aaecf7b665b14761b817a5
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-01-16 01:32:20 -0500

    new project

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import web1 from "../public/web1.png"
 import web2 from "../public/web2.png"
 import web3 from "../public/web3.png";
 import web4 from "../public/web4.png";
+import web5 from "../public/web5.png";
 import { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import { FaReact, FaJs, FaHtml5, FaCss3 } from 'react-icons/fa';
@@ -199,6 +200,29 @@ function App() {
                 ShopSmartPickk is a responsive Ecommerce website with a nice UX design and functioanlity.
                 </p>
               <img src={web4} className='rounded-xl object-cover ' />
+              <div className='flex justify-center mt-20 rounded-md shadow-lg bg-opacity-50	 bg-cyan-500 shadow-cyan-300/50 '>
+              {
+                isMd ? (<p className='text-white mt-1.5 text-lg'>Tools used:</p>): console.log("not md")
+              }
+                <div className='w-3/5  flex justify-center p-2 rounded-md'>
+                  <div className='mx-3 '><FaJs className='text-amber-300 size-7  '></FaJs></div>
+                  <div className='mx-3'><FaReact className='text-cyan-500	size-7 '></FaReact></div>
+                  <div className='mx-3 '><FaHtml5 className='text-orange-500	size-7 '></FaHtml5></div>
+                  <div className='mx-3'><FaCss3 className='text-blue-500	size-7 '></FaCss3></div>
+                  <div className='mx-3 '><img src={tailwind} className='size-7 max-w-lg'></img></div>
+                </div>
+              </div>
+          </div>
+
+          <div className='justify-center max-w-2xl basis-1/3 flex-1 shadow-2xl p-8 rounded-lg '>
+            <div className='py-2'>
+                  <button className='mx-2 w-24 h-10 rounded-lg bg-gradient-to-r from-cyan-300 to-teal-400  text-white  text-center '>Description</button>  
+                  <button className='mx-2 w-24 h-10 rounded-lg bg-gradient-to-r from-cyan-300 to-teal-400  text-white hover:transform hover:scale-110 transition-transform duration-300 ease-in-out center cursor-pointer text-center '><a href='https://iliaddh.github.io/bookStore/' target="_blank">Website</a></button>  
+                </div>
+                <p className='p-4 text-lg mb-6 '>
+                Book app is a responsive library website with a nice UX design and functioanlity.
+                </p>
+              <img src={web5} className='rounded-xl object-cover ' />
               <div className='flex justify-center mt-20 rounded-md shadow-lg bg-opacity-50	 bg-cyan-500 shadow-cyan-300/50 '>
               {
                 isMd ? (<p className='text-white mt-1.5 text-lg'>Tools used:</p>): console.log("not md")
