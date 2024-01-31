ab9833c1311e4f281abefbab53c24debff1cfa40
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-01-31 10:08:30 -0500

    spelling

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ function App() {
         <div className='text-center p-10'>
           <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Iliad Hemmatjoo</h2>
           <h3 className='text-2xl py-2 md:text-3xl'>Web Developer and Designer</h3>
-          <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto'>An enthusiast in web development field , and a computer scince student from Montreal</p>
+          <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto'>An enthusiast in web development field , and a Computer Science student from Montreal</p>
 
         </div>
         <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
