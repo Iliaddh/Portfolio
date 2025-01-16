3615169b14abfbe3715bf9efdb22f0fc0fdcf675
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2025-01-16 00:55:12 -0500

    domain issue

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/Portfolio/",
-  base:"/",
+  // base:"/",
   plugins: [react()],
   build: {
     outDir: 'build'
