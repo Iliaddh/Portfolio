bff1b190ada8c9633af705d6c00e46fd8cb18bab
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2025-01-16 00:53:24 -0500

    issue

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/Portfolio/",
+  base:"/",
   plugins: [react()],
   build: {
     outDir: 'build'
